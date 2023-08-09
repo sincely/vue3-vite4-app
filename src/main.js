1fbce683557d59dfc01600419eeca7f2dbb52fcc
成舟 <1738248438@qq.com>
2023-08-09 17:32:14 +0800

    测试

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+import App from './App.vue'
+// import router from './router' // 路由
+import './styles/index.less' // 全局样式
+// console.log('main.js')
+createApp(App).mount('#app')
