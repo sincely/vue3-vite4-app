bb42fff31553a7c35072e9b9747d014f96872009
成舟 <1738248438@qq.com>
2023-08-24 17:21:30 +0800

    update: 修改lint-staged配置

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   // 由于检查代码命令是对整个项目代码有效，有时候我们只想对自己改动的代码进行检查，而忽略项目其他代码。
   // 我们可以使用lint-staged，它可以让我们执行检查命令只对git缓存区的文件有效。
-  '*.{js,jsx,ts,tsx,vue}': ['prettier --write', 'eslint --fix']
+  '*.{js,jsx,ts,tsx,vue}': ['eslint --fix', 'prettier --write'],
+  '*.{css,less}': ['stylelint:fix', 'prettier --write']
 }
