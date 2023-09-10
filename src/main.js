ce5f908c7de5d1b65f400dab68efd0e044e1c593
成舟 <1738248438@qq.com>
2023-09-10 13:24:54 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import './style.css'
 import App from './App.vue'
 import router from './router' // 路由
 import './styles/index.less' // 全局样式
+console.log(11)
 createApp(App).use(router).mount('#app')
