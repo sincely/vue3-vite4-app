e0ec248178a2264160f0902134c05b27b72d02fb
成舟 <1738248438@qq.com>
2023-09-22 10:47:25 +0800

    update: 浏览器适配方案,分辨率适配

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <div id="app">
+  <div id="app" ref="app">
     <router-view />
   </div>
 </template>
+<script setup>
+import DevicePixelRatio from '@/utils/devicePixelRatio.js'
+
+onMounted(() => {
+  new DevicePixelRatio().init()
+})
+</script>
 
 <style scoped></style>
