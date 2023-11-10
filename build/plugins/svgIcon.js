e1584588d0375bc2170dcb5989c719af76bd0899
sincely.chang <sincely@puteyun.com>
2023-11-10 10:36:50 +0800

    fix: 增加静态资源

--- build/plugins/svgIcon.js
+++ build/plugins/svgIcon.js
@@ -1,10 +1,11 @@
+import { resolve } from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import svgLoader from 'vite-svg-loader'
 export default function svgIconPlugin() {
   return [
     createSvgIconsPlugin({
       // 配置路劲在你的src里的svg存放文件
-      iconDirs: [require.resolve('@icon')],
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
       // 指定symbolId格式
       symbolId: 'icon-[name]'
     }),
