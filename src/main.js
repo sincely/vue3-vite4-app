3f9431f7b22ab3b4381f451b19fdf9dc3234ddac
成舟 <1738248438@qq.com>
2023-11-23 16:01:58 +0800

    update: 更新main.js注释

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from '@/App.vue'
 import router from '@/router' // 路由
 import store from '@/store'
 import '@/styles/index.less' // 全局样式
-import 'virtual:svg-icons-register'
+import 'virtual:svg-icons-register' // svg图标注册
 
 const app = createApp(App)
 app.use(store).use(router)
