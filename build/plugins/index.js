8099b513a69a1064e26476664bd1eda6e271ec2c
成舟 <1738248438@qq.com>
2023-11-23 15:45:11 +0800

    update: 集成svg-icon

--- build/plugins/index.js
+++ build/plugins/index.js
@@ -11,13 +11,14 @@ import prefetchPlugin from './preload' // 预加载
 import webUpdateNoticePlugin from './updateVersion' // 版本更新
 import restartPlugin from './restart' // 重启服务
 import htmlPlugin from './html' // html插件
+import svgIconPlugin from './svgIcon' // svg图标集成
 /**
  * @description  创建vite插件
  * @param viteEnv - 环境变量配置
  * @param isBuild - 是否编译
  */
 export default function createVitePlugins(viteEnv, isBuild = false) {
-  const vitePlugins = [vue(), mock(viteEnv), ...unplugin(), commonjs(), restartPlugin()]
+  const vitePlugins = [vue(), mock(viteEnv), ...unplugin(), commonjs(), restartPlugin(), svgIconPlugin()]
   if (isBuild) {
     vitePlugins.push(compression(), legacy(), progress(), webUpdateNoticePlugin(), htmlPlugin())
   } else {
--- build/plugins/svgIcon.js
+++ build/plugins/svgIcon.js
@@ -4,11 +4,12 @@ import svgLoader from 'vite-svg-loader'
 export default function svgIconPlugin() {
   return [
     createSvgIconsPlugin({
-      // 配置路劲在你的src里的svg存放文件
-      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+      // 配置路径在你的src里的svg存放文件
+      iconDirs: [resolve(process.cwd(), 'src/icons/svg')],
       // 指定symbolId格式
-      symbolId: 'icon-[name]'
+      symbolId: 'icon-[dir]-[name]'
     }),
-    svgLoader()
+    /** 将 SVG 静态图转化为 Vue 组件 */
+    svgLoader({ defaultImport: 'url' })
   ]
 }
