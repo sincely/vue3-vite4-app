679c307de841e64612192621903913463120d73d
成舟 <1738248438@qq.com>
2023-12-13 10:58:57 +0800

    update: 引入element-plus图标库

--- src/plugins/icon.js
+++ src/plugins/icon.js
@@ -1,7 +1,7 @@
-import * as antdIcons from '@ant-design/icons-vue'
-// 全局注册antd图标
+import * as elementIcons from '@element-plus/icons-vue'
+// 全局注册element-plus图标
 export default function setupIcon(app) {
-  Object.keys(antdIcons).forEach((key) => {
-    app.component(key, antdIcons[key])
+  Object.keys(elementIcons).forEach((key) => {
+    app.component(key, elementIcons[key])
   })
 }
