62d292766f6320dc2e021ae4f40c482176beb673
成舟 <1738248438@qq.com>
2023-12-24 13:09:44 +0800

    update: 增加自定义指令

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import App from '@/App.vue'
 import store from '@/store'
 import router from '@/router' // 路由
 import '@/styles/index.less' // 全局样式
+import { setupDirectives } from './directives' // 自定义指令
 async function setupApp() {
   const app = createApp(App)
   app.use(store)
   app.use(router)
+  setupDirectives(app)
   app.mount('#app')
   app.config.performance = true
 }
